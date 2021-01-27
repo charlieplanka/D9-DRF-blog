2202307c43b9b3f5c7902bbe5d491ee3bd2cba36
Anastasia Naumenko <charlieplanka@gmail.com>
2021-01-27 18:39:13 +0300

    Add author

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from django.db import models  
-from django.utils import timezone  
+from django.utils import timezone
+from django.contrib.auth.models import User 
 
 
 class Post(models.Model):  
@@ -9,6 +10,7 @@ class Post(models.Model):
     content = models.TextField()
     updated = models.DateTimeField(default=timezone.now)  
     publication_date = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, null=True, default=None, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- app/views.py
+++ app/views.py
@@ -3,9 +3,10 @@ from app.serializers import PostSerializer
 from rest_framework import generics  
 
 
-class PostList(generics.ListAPIView):  
+class PostList(generics.ListCreateAPIView):  
     queryset = Post.objects.all()  
     serializer_class = PostSerializer
+
 
 class PostDetail(generics.RetrieveAPIView):  
     queryset = Post.objects.all()  
