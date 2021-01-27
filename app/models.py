73aea5576c8b1e4eacd735478d9d498e4c166e7a
Anastasia Naumenko <charlieplanka@gmail.com>
2021-01-27 16:25:54 +0300

    Add post model

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
-from django.db import models
+from django.db import models  
+from django.utils import timezone  
 
-# Create your models here.
+
+class Post(models.Model):  
+    title = models.CharField(max_length=255)  
+    slug = models.SlugField()
+    status = models.CharField(max_length=10, choices=[('D', 'draft'), ('P', 'published')])  
+    content = models.TextField()
+    updated = models.DateTimeField(default=timezone.now)  
+    publication_date = models.DateTimeField(default=timezone.now)
